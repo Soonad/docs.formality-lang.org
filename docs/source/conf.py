77ee04504cc3efb43791b0aa5528c85ec3415014
Maisa Milena <maisa.milena@gmail.com>
2019-08-29 23:25:45 +0000

    Fix bugs on Boxes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2019, Ethereum Foundation and Sunshine Cybernetics'
 author = u'Sunshine Cybernetics'
 
 # The short X.Y version
-version = u'3.91'
+version = u'3.95'
 # The full version, including alpha/beta/rc tags
-release = u'0.3.91'
+release = u'0.3.95'
 
 
 # -- General configuration ---------------------------------------------------
