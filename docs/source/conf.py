da302870d524890e5764211a636a21665de753de
Maisa <maisa.milena@gmail.com>
2019-10-31 14:53:29 -0300

    Update Tutorial 3 and 4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2019, Ethereum Foundation and Sunshine Cybernetics'
 author = u'Sunshine Cybernetics'
 
 # The short X.Y version
-version = u'3.143'
+version = u'3.146'
 # The full version, including alpha/beta/rc tags
-release = u'0.3.143'
+release = u'0.3.146'
 
 
 # -- General configuration ---------------------------------------------------
