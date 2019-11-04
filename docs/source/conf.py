bb7b931a5f1f743b7080ced76fd473d2ecd94a40
Maisa <maisa.milena@gmail.com>
2019-11-04 16:30:30 -0300

    Update Formalitu version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2019, Ethereum Foundation and Sunshine Cybernetics'
 author = u'Sunshine Cybernetics'
 
 # The short X.Y version
-version = u'3.146'
+version = u'0.3.157'
 # The full version, including alpha/beta/rc tags
-release = u'0.3.146'
+release = u'0.3.157'
 
 
 # -- General configuration ---------------------------------------------------
