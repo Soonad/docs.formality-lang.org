398e8985643fbeddf3776e78f97d44bb79e7811d
Maisa <maisa.milena@gmail.com>
2019-11-05 19:15:42 -0300

    Updates to v.3.161

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2019, Ethereum Foundation and Sunshine Cybernetics'
 author = u'Sunshine Cybernetics'
 
 # The short X.Y version
-version = u'0.3.157'
+version = u'3.161'
 # The full version, including alpha/beta/rc tags
-release = u'0.3.157'
+release = u'0.3.161'
 
 
 # -- General configuration ---------------------------------------------------
